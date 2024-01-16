e727598c60a8b88448fda80c5e4214b97663da20
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-16 23:15:41 +0100

    pycodestyle

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in  range(ord('a'), (ord('z') + 1)):
-    print("{}" .format(chr(character)), end = "")
+for character in range(ord('a'), (ord('z') + 1)):
+    print("{}" .format(chr(character)), end="")
