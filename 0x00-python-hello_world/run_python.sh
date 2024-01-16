916f82e12c112b68e3a3ba9c8b4bfcdec138ae18
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-16 09:20:58 +0100

    concated?

---
+++ 0x00-python-hello_world/run_python.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set up environment if needed
+# export PATH=$PATH:/path/to/additional/binaries
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/additional/libraries
+
+# Execute the Python script
+"/c/Users/efeto/AppData/Local/Programs/Python/Python312/python.exe" "$@"
+
