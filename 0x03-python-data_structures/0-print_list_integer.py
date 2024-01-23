0e1e9b707b5ed3aa3dd075f225bffb736bd874f7
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-23 16:55:43 +0100

    newline?

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     listlen = len(my_list)
     a = 0
     while a < listlen:
-        print("{}".format(my_list[a]))
+        print("{}\n".format(my_list[a]))
         a += 1
