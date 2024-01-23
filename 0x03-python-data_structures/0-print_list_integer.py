c6dc9dbef8345d8273875c388e144f7007bf1fad
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-23 17:04:39 +0100

    used for loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    listlen = len(my_list)
-    a = 0
-    while a < listlen:
-        print("{}\n".format(my_list[a]))
-        a += 1
+    for num in my_list:
+        print("{}".format(num))
