9db84e06d645f098a992ab1ecf96c774bedccf4f
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-24 13:15:44 +0100

    made it executable

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     idx = -1
     lilen = len(my_list)
     while idx >= -lilen:
-        print("{}". format(my_list[idx]))
+        print("{}".format(my_list[idx]))
         idx -= 1
