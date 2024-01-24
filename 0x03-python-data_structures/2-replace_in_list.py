704a91114cc9cd1810dfc81c5b0899fb4b1dc3e3
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-24 12:52:58 +0100

    created function to replace list element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def replace_in_list(my_list, idx, element):
+    lilen = len(my_list)
+    if idx < 0 or idx >= lilen:
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
