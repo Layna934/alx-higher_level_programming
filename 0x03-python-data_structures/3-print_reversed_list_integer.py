4ca0a78cd8b12912ce3f3ff830161bd5711d406f
PRECIOUS UMUKORO <Eseprecious934@gmail.com>
2024-01-24 13:14:04 +0100

    created function to print a reverse list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    idx = -1
+    lilen = len(my_list)
+    while idx >= -lilen:
+        print("{}". format(my_list[idx]))
+        idx -= 1
