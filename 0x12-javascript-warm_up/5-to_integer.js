4c87e4f6dafcccf96b6a91db395490272b4f13ee
0xPrecious <eseprecious934@gmail.com>
2024-04-22 20:07:55 -0700

    Number issue in 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const numArg = Math.floor(process.argv[2]);
+const numArg = Math.floor(Number(process.argv[2]));
 console.log(isNaN(numArg) ? 'Not a Number' : `My Number: ${numArg}`);
