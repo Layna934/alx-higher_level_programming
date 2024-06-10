f576a9978f4c83716c36c4998dc25f525446e718
0xPrecious <eseprecious934@gmail.com>
2024-06-09 22:49:25 -0700

    square with private class attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Square with private instance attribute"""
+
+
+class Square:
+    """Non-empty class"""
+
+    def __init__(self, size):
+        """
+        Instance of Square
+        With private attribute
+        """
+
+        self.__size = size
