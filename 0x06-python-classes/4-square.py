5384560f50ab83bc045075a4d28a0fec088d1fe1
0xPrecious <eseprecious934@gmail.com>
2024-06-10 20:06:55 -0700

    documentation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-
+"""
+creating square class
+"""
 class Square:
+    """
+    setting attributes and methods for class
+    """
     def __init__(self, size=0):
+        """Initializing the square"""
         self.size = size
 
-    @propery
+    @property
+    """size getter"""
     def size(self):
         return(self.__size)
 
     @size.setter
+    """size setter"""
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -17,5 +25,6 @@ class Square:
         self.__size = value
 
     def area(self):
+        """calculating area of square"""
         area = self.__size * self.__size
         return area
