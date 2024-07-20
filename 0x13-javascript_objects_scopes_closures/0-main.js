18755c49348f66cbe0b670f9240123b23c7b5a2a
0xPrecious <eseprecious934@gmail.com>
2024-07-20 16:29:40 -0700

    rectangle 2

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,4 +1,3 @@
-
 #!/usr/bin/node
 const Rectangle = require('./0-rectangle');
 
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // class to define width and height
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
